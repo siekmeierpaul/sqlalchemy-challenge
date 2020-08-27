175d158137c4574a231082a6357f8794c16bfdff
Paul Siekmeier <paul.siekmeier@jamf.com>
2020-08-27 15:36:40 -0500

    that was step one

---
+++ app.py
@@ -0,0 +1,88 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+
+# Save reference to the table
+station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/station"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all prcp"""
+    # Query all measurements
+    results = session.query(measurement.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    # all_names = list(np.ravel(results))
+
+    # return jsonify(all_names)
+    return results
+
+@app.route("/api/v1.0/station")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of stations"""
+    # Query all stations
+    results = session.query(station.name).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    # all_passengers = []
+    # for name, age, sex in results:
+    #     passenger_dict = {}
+    #     passenger_dict["name"] = name
+    #     passenger_dict["age"] = age
+    #     passenger_dict["sex"] = sex
+    #     all_passengers.append(passenger_dict)
+
+    # return jsonify(all_passengers)
+    return results
+
+if __name__ == '__main__':
+    app.run(debug=True)
